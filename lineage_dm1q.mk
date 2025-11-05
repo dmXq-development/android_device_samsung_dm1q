64d1bca44c202fd85c70a3079f5bbb713aa44803
Mezaque Silver <mezaquegit@gmail.com>
2025-11-04 22:26:20 -0300

    dm1q: Update blobs from S911BXXS8DYI4
    Change-Id: I6ed69d2b36a454f2d41e2ca4d08423eb5cc90cb3

--- lineage_dm1q.mk
+++ lineage_dm1q.mk
@@ -23,7 +23,7 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="dm1qxxx-user 15 AP3A.240905.015.A2 S911BXXS8DYG5 release-keys" \
-    BuildFingerprint=samsung/dm1qxxx/dm1q:13/TP1A.220624.014/S911BXXS8DYG5:user/release-keys \
+    BuildDesc="dm1qxxx-user 15 AP3A.240905.015.A2 S911BXXS8DYI4 release-keys" \
+    BuildFingerprint=samsung/dm1qxxx/dm1q:13/TP1A.220624.014/S911BXXS8DYI4:user/release-keys \
     DeviceProduct=dm1q \
     SystemName=dm1q
